b3dfc86c580c22f7c67b75824104443c6e194252
Renchiiks <renate.kalmikova@gmail.com>
2020-10-01 18:04:56 +0300

    Creating POJO's. Closes #2.

---
+++ src/main/java/com/example/petclinic/model/Pet.java
@@ -0,0 +1,11 @@
+package com.example.petclinic.model;
+
+
+
+import java.util.Date;
+
+public class Pet {
+    private Owner owner;
+    private PetType petType;
+    private Date birthDate;
+}
---
+++ src/main/java/com/example/petclinic/model/PetType.java
@@ -0,0 +1,5 @@
+package com.example.petclinic.model;
+
+public class PetType {
+    private String name;
+}
