423790103e523e6f16609696668781cc686d9085
Renchiiks <renate.kalmikova@gmail.com>
2020-10-02 16:32:47 +0300

    adding BaseEntity for Id. Closes #10

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
@@ -1,10 +1,8 @@
 package com.example.petclinic.model;
 
-
-
 import java.util.Date;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private Owner owner;
     private PetType petType;
     private Date birthDate;
--- pet-clinic-data/src/main/java/com/example/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/PetType.java
@@ -1,5 +1,5 @@
 package com.example.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 }
