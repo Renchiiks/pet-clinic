cb761b9b586bfbdbc3fa478aec35f7465449a3f0
Renchiiks <renate.kalmikova@gmail.com>
2020-10-12 14:06:04 +0300

    Added getters and setters. Where missing some

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
@@ -6,4 +6,28 @@ public class Pet extends BaseEntity {
     private Owner owner;
     private PetType petType;
     private Date birthDate;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
