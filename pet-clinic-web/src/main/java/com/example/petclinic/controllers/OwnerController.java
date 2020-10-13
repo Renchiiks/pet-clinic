ed7517ab082f462aa914b1ed2a7ee54539f192bc
Renchiiks <renate.kalmikova@gmail.com>
2020-10-13 15:28:07 +0300

    List Owners Closes #18

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
@@ -1,14 +1,29 @@
 package com.example.petclinic.controllers;
 
+import com.example.petclinic.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String ownerList() {
-        return "owner/index";
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
     }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
+        return "owners/index";
+
+    }
+
+
 }
