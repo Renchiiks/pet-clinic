7f7fbe47e19e5b6f6643fae363ebe9ffebd8925d
Renchiiks <renate.kalmikova@gmail.com>
2020-10-21 15:37:28 +0300

    Created tests for VisitServiceMap corrections.

--- pet-clinic-data/src/test/java/com/example/petclinic/service/map/VisitServiceMapTest.java
+++ pet-clinic-data/src/test/java/com/example/petclinic/service/map/VisitServiceMapTest.java
@@ -1,5 +1,7 @@
 package com.example.petclinic.service.map;
 
+import com.example.petclinic.model.Owner;
+import com.example.petclinic.model.Pet;
 import com.example.petclinic.model.Visit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,12 +14,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class VisitServiceMapTest {
     VisitServiceMap visitServiceMap;
     Long id = 1L;
+    Owner owner = Owner.builder().id(id).build();
+    Pet pet = Pet.builder().owner(owner).id(id).build();
 
     @BeforeEach
     void setUp() {
         visitServiceMap = new VisitServiceMap();
 
-        Visit visit = Visit.builder().id(id).build();
+        Visit visit = Visit.builder().id(id).pet(pet).build();
 
         visitServiceMap.save(visit);
     }
@@ -46,7 +50,8 @@ class VisitServiceMapTest {
     @Test
     void saveWithExistingId() {
         Long id = 2L;
-        Visit visit = Visit.builder().id(id).build();
+        Visit visit = Visit.builder().id(id).pet(pet).build();
+
         Visit savedVisit = visitServiceMap.save(visit);
 
         assertEquals(id, savedVisit.getId());
@@ -54,7 +59,7 @@ class VisitServiceMapTest {
 
     @Test
     void saveWithNoId() {
-        Visit visit = Visit.builder().build();
+        Visit visit = Visit.builder().pet(pet).build();
         visitServiceMap.save(visit);
 
         assertNotNull(visit);
