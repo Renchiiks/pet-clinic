0a79a8b49ccead5f31c28aeacee896fc4b3bf206
Renchiiks <renate.kalmikova@gmail.com>
2020-10-21 13:50:55 +0300

    Created tests for SpecialityServiceMap.

--- pet-clinic-data/src/test/java/com/example/petclinic/service/map/SpecialityServiceMapTest.java
+++ pet-clinic-data/src/test/java/com/example/petclinic/service/map/SpecialityServiceMapTest.java
@@ -4,6 +4,12 @@ import com.example.petclinic.model.Speciality;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class SpecialityServiceMapTest {
 
     SpecialityServiceMap specialityServiceMap;
@@ -19,21 +25,46 @@ class SpecialityServiceMapTest {
 
     @Test
     void findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+
+        assertEquals(1, specialityServiceMap.findAll().size());
     }
 
     @Test
     void deleteById() {
+        specialityServiceMap.deleteById(id);
+
+        assertEquals(0, specialityServiceMap.findAll().size());
     }
 
     @Test
     void delete() {
+        specialityServiceMap.delete(specialityServiceMap.findById(id));
+
+        assertEquals(0, specialityServiceMap.findAll().size());
     }
 
     @Test
-    void save() {
+    void saveWithNoId() {
+        Speciality speciality = Speciality.builder().build();
+        specialityServiceMap.save(speciality);
+
+        assertNotNull(speciality);
+        assertNotNull(speciality.getId());
+    }
+
+    @Test
+    void saveWithExistingId() {
+        Speciality speciality2 = Speciality.builder().id(2L).build();
+        Speciality savedSpeciality = specialityServiceMap.save(speciality2);
+
+        assertEquals(id, savedSpeciality.getId());
     }
 
     @Test
     void findById() {
+        Speciality speciality = specialityServiceMap.findById(id);
+
+        assertEquals(id, speciality.getId());
     }
 }
